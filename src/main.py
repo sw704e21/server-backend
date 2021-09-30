0cd60e01580e06a21bda3b61ad4fd927fe566ab7
AdrianPlesner <Adrian.plesner@gmail.com>
2021-09-30 11:15:38 +0200

    test with tests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 import numpy as np
+import pytest
 
 print("hello world")
 a = np.zeros(50)
+
+
+def inc(x):
+    return x + 1
+
+
+def test_inc():
+    assert inc(3) == 4
---
+++ unittest/test.py
@@ -0,0 +1,7 @@
+import pytest
+import main.py
+
+
+
+
+
