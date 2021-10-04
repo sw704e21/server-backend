f5045ade8297f06c64ef6ce1b2b08f0d2a30c67f
AdrianPlesner <Adrian.plesner@gmail.com>
2021-10-04 09:37:38 +0200

    satisfy linter

--- src/some.py
+++ src/some.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pytest
+
 
 print("hello world")
 a = np.zeros(50)
@@ -7,4 +7,3 @@ a = np.zeros(50)
 
 def inc(x):
     return x + 1
-
--- tests/test__some.py
+++ tests/test__some.py
@@ -1,4 +1,4 @@
-import pytest
+
 from src import some
 
 
