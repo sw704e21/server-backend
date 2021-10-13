5315b86eedea54dd07abb4eb89d356c24a67bcff
JohanKrogh <johan.krogh1@gmail.com>
2021-10-13 15:10:52 +0200

    Lint

--- src/SentimentAnalyzer.py
+++ src/SentimentAnalyzer.py
@@ -1,5 +1,4 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
-from pprint import pprint
 
 
 class SentimentAnalyzer():
@@ -7,20 +6,15 @@ class SentimentAnalyzer():
         self.data = data
         self.queue = queue
 
-    def AnalyzePosts(self, headline, postText):
+    def AnalyzePosts(self, headline, posttext):
         sia = SIA()
 
         headline_score = sia.polarity_scores(headline)
         headline_score['headline'] = headline
 
-        postText_score = sia.polarity_scores(postText)
-        postText_score['post text'] = postText
+        posttext_score = sia.polarity_scores(posttext)
+        posttext_score['post text'] = posttext
 
-        return (headline_score, postText_score)
+        return headline_score, posttext_score
 
-"""sa = SentimentAnalyzer([], [])
-headline = "The next bitcoin halving will be in approximately 909 days. Keep on stacking my friends while bitcoin is cheap."
-postText = "Quick question. I have money on my cash app with Bitcoin. Can I go to a Bitcoin atm and withdraw cash with my cash app btc wallet address?', 'What you think about binance"
 
-sa.AnalyzePosts(headline, postText)
-"""
