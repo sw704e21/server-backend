ac67a26b28e11ab79a30e73c1a988d55c65c5339
social2dk <45236436+social2dk@users.noreply.github.com>
2021-10-13 13:49:26 +0200

    Implement queue and terminate message to server

--- src/queueserver.py
+++ src/queueserver.py
@@ -1,16 +1,24 @@
 import socket
+import queue
+import sys
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+queue = queue.Queue()
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                if data.decode('utf_8') == 'terminate':
+                    sys.exit()
+                queue.put(data)
+                conn.sendall(b'code 100')
