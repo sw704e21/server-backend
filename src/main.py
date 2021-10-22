5b971b10937507eff306f4a470bb9862cb0228d0
AdrianPlesner <Adrian.plesner@gmail.com>
2021-10-22 13:34:20 +0200

    fix multiprocesses

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ import time
 
 # A process which starts dequeue processes periodically if queue is non-empty
 def manage_processes(server, process_delay):
-    p = Process(target=start_dequeue_process, args=(server,))
     while True:
+        p = Process(target=start_dequeue_process, args=(server,))
         # If something is in queue, start a new process
         if server.queue.qsize() > 0:
             print('Starting new thread')
@@ -18,6 +18,7 @@ def manage_processes(server, process_delay):
 def start_dequeue_process(server):
     while server.queue.qsize() > 0:
         server.dequeue()
+    print('Now closing process')
 
 
 if __name__ == '__main__':
