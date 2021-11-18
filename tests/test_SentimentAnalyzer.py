58eccfefc58065aaaae5615b3e06f490d046fba7
social2dk <45236436+social2dk@users.noreply.github.com>
2021-11-18 11:13:24 +0100

    Test updated

--- tests/test_SentimentAnalyzer.py
+++ tests/test_SentimentAnalyzer.py
@@ -13,8 +13,9 @@ def testAnalyzePost():
     post_text = "Quick question. I have money on my cash app with Bitcoin. " \
                 "Can I go to a Bitcoin atm and withdraw cash with my cash" \
                 " app btc wallet address?', 'What you think about binance"
+    full_text = headline + post_text
 
-    result = sa.analyze_posts(headline, post_text)
+    result = sa.analyze_posts(full_text)
 
     headline_score = sia.polarity_scores(headline)
     headline_score['headline'] = headline
