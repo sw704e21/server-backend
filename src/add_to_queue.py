c75f1af1af9879a8c1a71c77df00d60177a79004
AdrianPlesner <Adrian.plesner@gmail.com>
2021-11-29 11:20:57 +0100

    revert debugging code

--- src/add_to_queue.py
+++ src/add_to_queue.py
@@ -1,12 +1,11 @@
 from processingqueue.queueclient import QueueClient
 import sys
-import json
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 0:
-        #m = sys.argv[1]
-        m = {"title": "testtitle", "selftext": "testtext", "created_utc": 1234, "permalink": "testUrl", "source": "testcoin", "score": 1234, "num_comments": 123}
+        m = sys.argv[1]
         client = QueueClient()
-        client.send(json.dumps(m))
+        client.send(m)
     else:
         print('Missing message')
