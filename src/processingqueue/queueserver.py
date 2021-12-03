7e24ff373717047b6b2647d84ad2a157e429d373
AdrianPlesner <Adrian.plesner@gmail.com>
2021-12-03 16:16:30 +0100

    queueserver use kafka

--- src/processingqueue/queueserver.py
+++ src/processingqueue/queueserver.py
@@ -1,77 +1,36 @@
-import socket
-import sys
-import pickle
-import time
 import multiprocessing
 from SentimentAnalyzer import SentimentAnalyzer
+import kafka
+import pickle
 import logging
 logger = logging.getLogger("backend")
 
 
 class QueueServer:
-    def __init__(self, host='0.0.0.0', port=65432):
-        self.host = host
-        self.port = port
+    def __init__(self):
+        self.host = "104.41.213.247"
+        self.port = "9092"
+        self.server = self.host + ":" + self.port
+        self.api_version = (2, 4, 0)
+        self.topic = "PostsToProcess"
         self.queue = multiprocessing.Queue()
 
-    def _recv_timeout(self, the_socket, timeout=2):
-        # make socket non blocking
-        the_socket.setblocking(0)
-
-        # total data partwise in an array
-        total_data = []
-        data = ''
-
-        # beginning time
-        begin = time.time()
-        while 1:
-            # if you got some data, then break after timeout
-            if total_data and time.time() - begin > timeout:
-                break
-
-            # if you got no data at all, wait a little longer, twice the timeout
-            elif time.time() - begin > timeout * 2:
-                break
-
-            # recv something
-            try:
-                data = the_socket.recv(1024 * 8)
-                if data:
-                    total_data.append(data)
-                    # change the beginning time for measurement
-                    begin = time.time()
-                else:
-                    # sleep for sometime to indicate a gap
-                    time.sleep(0.1)
-            except socket.error:
-                # Catch non-blocking socket exception as part of timeout functionality
-                pass
-
-        # join all parts to make final bytestring
-        return b''.join(total_data)
-
     def run(self):
-        logger.info(f'QueueServer now listening on {self.host}:{self.port}')
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self.host, self.port))
+        adm = kafka.KafkaAdminClient(bootstrap_servers=self.server, api_version=self.api_version)
+        consumer = kafka.KafkaConsumer(self.topic, bootstrap_servers=self.server, api_version=self.api_version,
+                                       group_id="readposts")
+        if self.topic not in adm.list_topics():
+            logger.error(f"Topic {self.topic} not present at kafka server")
+            exit(1)
 
-            while True:
-                s.listen()
-                conn, addr = s.accept()
-                with conn:
-                    logger.debug(f'Connected by {addr}')
-                    data = self._recv_timeout(conn)
-                    logger.debug(f"Received {len(data)}")
-                    if not data:
-                        conn.sendall(pickle.dumps('Error receiving data'))
-                        logger.error("Error receiving data")
-                        break
-                    if pickle.loads(bytes(data)) == 'terminate':
-                        conn.sendall(pickle.dumps('Terminating server'))
-                        logger.debug("Terminating server")
-                        sys.exit()
-                    self.queue.put(pickle.loads(data))
-                    conn.sendall(pickle.dumps('Data put into queue'))
+        logger.info(f"Start listening to {self.topic}")
+        for p in consumer:
+            try:
+                logger.info("Received data")
+                data = p.value
+                self.queue.put(data.decode('utf-8'))
+            except Exception as e:
+                logger.error(e)
 
     def dequeue(self):
         # Takes item from queue and hands it to sentiments analyzer, called by processes from main
