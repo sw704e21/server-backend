727bb453f783590d6cf98e53d9df4f8812846b88
AdrianPlesner <Adrian.plesner@gmail.com>
2021-12-10 11:18:55 +0100

    custom process count

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from processingqueue.queueserver import QueueServer
-from multiprocessing import Process, Manager, Value
+from multiprocessing import Process, Manager, Value, cpu_count
 from calculate_scores import ScoreCalculator
 import time
 import logging
@@ -21,13 +21,16 @@ logger.addHandler(handler)
 manager = Manager()
 dequeue_count = Value('i', 0, lock=True)
 
+# It does not make sense to make more dequeue processes than there are cpu cores.
+max_process_count = cpu_count()
+
 
 # A process which starts dequeue processes periodically if queue is non-empty
 def manage_processes(server, process_delay):
     while True:
         p = Process(target=start_dequeue_process, args=(server,))
         # If something is in queue, start a new process
-        if server.queue.qsize() > 0 and dequeue_count.value < 10:
+        if server.queue.qsize() > 0 and dequeue_count.value < max_process_count:
             logger.info('Starting new dequeue process')
             p.start()
             with dequeue_count.get_lock():
